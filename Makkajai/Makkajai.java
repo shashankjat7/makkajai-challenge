1c9bfc1378adef93437f41bfa4c11303c2b51ba8
Shashank <shashankjat7@gmail.com>
2023-03-26 10:51:24 +0530

    Delete Makkajai directory

--- Makkajai/Makkajai.java
+++
@@ -1,9 +0,0 @@
-
-public class Makkajai {
-    public static void main(String[] args) {
-        GameOfLife gameOfLife = new GameOfLife(10);
-        gameOfLife.tick();
-        System.out.print("\nLive Coordinates after one tick are : ");
-        gameOfLife.printLiveCellCoordinates();
-    }
-}
